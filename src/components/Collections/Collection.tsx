07e8dcf68f4b60ab4b997ca819e59910d2ef2575
pc <pranavchandra27@gmail.com>
2022-02-06 18:32:20 +0530

    no picture fix

--- src/components/Collections/Collection.tsx
+++ src/components/Collections/Collection.tsx
@@ -15,29 +15,29 @@ const Collection = ({ collection }: Props) => {
           <div className="h-72 rounded-md overflow-hidden relative collection">
             <div className="hidden group-hover:block transform transition-transform duration-300 ease-in-out absolute left-0 top-0 w-full h-full bg-white bg-opacity-20 cursor-pointer" />
             <div className="flex h-full">
-              <div className="overflow-hidden w-[70%]">
+              <div className="overflow-hidden w-[70%] bg-gray-200">
                 <img
                   role="presentation"
-                  className="h-full w-full object-cover"
-                  src={collection.preview_photos[0].urls.small}
-                  alt={collection.preview_photos[0].alt_description}
+                  className="h-full w-full object-cover "
+                  src={collection.preview_photos[0]?.urls.small}
+                  alt={collection.preview_photos[0]?.alt_description}
                 />
               </div>
               <div className="overflow-hidden h-full ml-0.5 w-[30%]">
-                <div className="h-1/2 mb-0.5">
+                <div className="h-1/2 mb-0.5 bg-gray-200">
                   <img
                     role="presentation"
                     className="h-full w-full object-cover"
-                    src={collection.preview_photos[1].urls.small}
-                    alt={collection.preview_photos[1].alt_description}
+                    src={collection.preview_photos[1]?.urls.small}
+                    alt={collection.preview_photos[1]?.alt_description}
                   />
                 </div>
-                <div className="h-1/2">
+                <div className="h-1/2 bg-gray-200">
                   <img
                     role="presentation"
                     className="h-full w-full object-cover"
-                    src={collection.preview_photos[2].urls.small}
-                    alt={collection.preview_photos[2].alt_description}
+                    src={collection.preview_photos[2]?.urls.small}
+                    alt={collection.preview_photos[2]?.alt_description}
                   />
                 </div>
               </div>
